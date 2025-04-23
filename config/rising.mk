32927699fb918f83c621597438d2cf0fe30facdd
slogar25 <michaelslogar@gmail.com>
2025-04-23 00:20:17 +0000

    rising: change back to Rising Gapps and pixeloverlays

--- config/rising.mk
+++ config/rising.mk
@@ -1,4 +1,8 @@
 -include vendor/addons/config.mk
+WITH_GMS ?= false
+ifeq ($(WITH_GMS),true)
+-include vendor/gms/products/gms.mk
+endif
 include vendor/rising/config/properties.mk
 include vendor/rising/config/packages.mk
 include vendor/rising/config/version.mk
